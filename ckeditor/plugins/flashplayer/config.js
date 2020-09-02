6589c08c86452fa3d64024c817da7530a4bbb8ef
Jan-Paul Kleijn <janpaul.kleijn@gmail.com>
2020-09-02 14:05:26 +0200

    Add files via upload

---
+++ ckeditor/plugins/flashplayer/config.js
@@ -0,0 +1,9 @@
+/**************************************
+    Webutler V2.1 - www.webutler.de
+    Copyright (c) 2008 - 2011
+    Autor: Sven Zinke
+    Free for any use
+    Lizenz: GPL
+**************************************/
+
+var playerConf={color:'6DC018',mediapath:'userfiles/mp3',playerpath:'ckeditor/player'};
