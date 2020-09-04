b77af773aebed1e9a9833a2acb44b68348186c91
Jan-Paul Kleijn <janpaul.kleijn@gmail.com>
2020-09-04 13:39:39 +0200

    Add files via upload

---
+++ ckeditor/plugins/youtube/lang/nl.js
@@ -0,0 +1,9 @@
+CKEDITOR.plugins.setLang('youtube', 'nl',
+{
+  youtube : 
+  {
+    title : "Youtube Video invoegen",
+    button : "Youtube Video invoegen",
+    pasteMsg : "<p>Hier kunt je de identificatiecode van het Youtube filmfragment invoeren<br />Deze identificatiecode is de unieke code die je vindt in een link naar het Youtube filmfragment.<br />In de link <em class='gray'>https://www.youtube.com/watch?v=<strong>v9g1yRXF8I8</strong></em> vindt je de unieke code <em class='value'>v9g1yRXF8I8</em>. Dit is wat bedoelt wordt met de identificatiecode van het YouTube filmfragment."
+  }
+});
